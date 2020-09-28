df3dd2b78288740f78c5d62d354ae6d9eabe2220
LIvingston <la@todoo.co>
2020-09-28 09:03:52 -0500

    test [ADD] cambios cost_sale_order

--- cost_sale_order/models/sale_order.py
+++ cost_sale_order/models/sale_order.py
@@ -1,3 +1,4 @@
+from odoo.exceptions import AccessError, UserError, RedirectWarning, ValidationError, Warning
 from odoo import models,fields,api
 
 class SaleOrder(models.Model):
@@ -8,3 +9,9 @@ class SaleOrder(models.Model):
     unit_price = fields.Float('Unit Price', tracking=True)
     price_n_iva = fields.Float('Price Unit without IVA', tracking=True)
     contribution_percentage = fields.Float('Contribution Percentage', tracking=True)
+
+    @api.depends('partner_id')
+    def partner_id(self):
+        for record in self:
+            if record.partner_id:
+                raise Warning(_("Invoice Condition: %s" % record.partner_id.invoice_condition))
