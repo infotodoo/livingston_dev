ad2fe9be82054d64b2f46ff3d7f536f746b8377e
LIvingston <la@todoo.co>
2020-10-15 11:33:37 -0500

    cambios zpp

--- zpp_report/__manifest__.py
+++ zpp_report/__manifest__.py
@@ -16,7 +16,7 @@
     'version': '13.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['mrp_suprapak','contacs_thomas'],
+    'depends': ['mrp_suprapak'],
   
     # always loaded
     'data': [
