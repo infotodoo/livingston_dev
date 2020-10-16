b21acff93b70d21941f45b6e7ca242e65af12be5
LIvingston <la@todoo.co>
2020-10-15 21:49:37 -0500

    test 1

---
+++ api_rest/services/__init__.py
@@ -0,0 +1,6 @@
+from . import invoice_service
+from . import partner_service
+from . import product_service
+from . import partner_service_test
+from . import city_service
+from . import country_service
