ea8f9f417ecd5f39af6afa1c84e97c27a07702d6
LIvingston <la@todoo.co>
2020-10-23 16:30:57 -0500

    prueba

---
+++ stock_report_app/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Cuadro de Stock",
+    "version": "13.0.1.0.0",
+    'depends': ['stock', 'web_dashboard', 'web_cohort', 'web_map', 'web_grid', 'sale_stock_app','product'],
+    "category": "Warehouse Management",
+    "installable": True,
+    "license": "AGPL-3",
+    "data": [
+        "security/ir.model.access.csv",
+        #"report/stock_enterprise_templates.xml",
+        "report/report_stock_availability.xml",
+        "views/product_view.xml",
+        "report/stock_report_view.xml",
+    ],
+}
