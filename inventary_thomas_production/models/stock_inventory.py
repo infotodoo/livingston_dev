e28c664c2e54a1b4320c3cce407d158051b72ae5
LIvingston <la@todoo.co>
2020-11-04 22:52:27 -0500

    cambios

--- inventary_thomas_production/models/stock_inventory.py
+++ inventary_thomas_production/models/stock_inventory.py
@@ -5,3 +5,6 @@ class StockInventory(models.Model):
     _inherit = 'stock.inventory'
 
     diff_motive = fields.Char('Difference Reason')
+    assigned_to = fields.Many2one('res.users','Assigned to')
+    start_date = fields.Date('Date start programming')
+    end_date = fields.Date('Date snd programming')
