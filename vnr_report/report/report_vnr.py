c54fdb5d8e7de13a52663dc4d2239a48ab3cb83a
LIvingston <la@todoo.co>
2020-11-24 07:38:33 -0500

    [ADD] cambios vnr report

--- vnr_report/report/report_vnr.py
+++ vnr_report/report/report_vnr.py
@@ -15,25 +15,27 @@ class ZppReportLine(models.Model):
     
     name = fields.Char('Mayor Cte',readonly=True)
     default_code = fields.Char('Material',readonly=True)
-    description = fields.Char('Material',readonly=True)
+    description = fields.Char('Description',readonly=True)
     total_stock = fields.Float('Total Stock',readonly=True)
     total = fields.Float('Total Value',readonly=True)
     unit_price = fields.Float('Unit Value',readonly=True)
+    price_unit = fields.Float('Sale Value Unitary',readonly=True)
+    sale_price = fields.Float('Sale price',readonly=True)
    
     
     def init(self):
         tools.drop_view_if_exists(self._cr, 'report_vnr')
         query = """
-        CREATE or REPLACE VIEW report_zpp AS(
+        CREATE or REPLACE VIEW report_vnr AS(
         
         select 
-        row_number() OVER (ORDER BY id) as id,
-        (
-            select aa.code from
-            account_account aa letf join product_category pc 
-            on (pc.property_account_creditor_price_difference_categ = aa.id)
-            where pp.categ_id = pc.id
-        )as name,
+        row_number() OVER (ORDER BY pp.id) as id,
+        --(
+          --  select aa.code from
+            --account_account aa letf join product_category pc 
+            --on (pc.property_account_creditor_price_difference_categ = aa.id)
+            --where pp.categ_id = pc.id
+        --)as name,
         pp.default_code,
         (
             select pt.name from
@@ -54,9 +56,19 @@ class ZppReportLine(models.Model):
             select sum(svl.value)/sum(svl.quantity)
             from stock_valuation_layer svl
             where pp.id = svl.product_id
-        )as unit_price
+        )as unit_price,
+        --(
+          --  select sol.price_unit
+            --from sale_order_line sol
+            --where pp.id = sol.product_id
+        --)as price_unit
+        (
+            select sum(ppi.fixed_price)/count(ppi.product_id)
+            from product_pricelist_item ppi
+            where pp.id = ppi.product_id
+        )as sale_price
         from product_product pp
-        left join stock_valuation_layer svl on (svl.product_id = pp.id)
+        --left join stock_valuation_layer svl on (svl.product_id = pp.id)
         where 1=1
         );
         """
