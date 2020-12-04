b548bc3bb4e04d9e3a4ea5d27614f321979fc6c3
la-todoo <la@todoo.co>
2020-12-04 15:14:41 -0500

    [ADD] init

--- api_rest/services/__init__.py
+++ api_rest/services/__init__.py
@@ -10,7 +10,7 @@ from . import mrp_workcenter_productivity_loss
 from . import product_tracking_service
 from . import stock_production_service
 from . import workorder_service
-from . import production_service
+from . import lot_update_service
 #from . import workorder_users
 from . import workcenter_service
 from . import workcenter_loss_service
