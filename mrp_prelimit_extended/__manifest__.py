bfeac3d39f0d119f078feb0049e417ad93190784
Livingston <la@todoo.co>
2020-12-26 17:54:27 -0500

    [ADD] prelimit_extended

---
+++ mrp_prelimit_extended/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Mrp Prelimit Extended",
+
+    'summary': "Mrp Prelimit Extended",
+
+    'description': "Mrp Prelimit Extended",
+
+    'author': "Todoo SAS",
+    'contributors': "Livingston Arias Narváez la@todoo.co",
+    'website': "http://www.todoo.co",
+    'category': 'Accounting',
+    'version': '13.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['account'],
+  
+    # always loaded
+    'data': [
+        
+    ],
+}
