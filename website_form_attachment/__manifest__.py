344972cafc62c6d782ad5e41a24e90a289ba271d
livingston <la@todoo.co>
2021-01-24 03:19:07 +0000

    cambio

--- website_form_attachment/__manifest__.py
+++ website_form_attachment/__manifest__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # Copyright 2019 Artem Shurshilov
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+#test
 
 {
     'name': 'Website attachments contact us form',
