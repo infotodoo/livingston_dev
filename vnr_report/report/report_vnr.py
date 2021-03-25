1df53dad29e294d991c90fb583b5790acf810528
Your Name <you@example.com>
2021-03-25 16:36:27 +0000

    [ADD] cambio vnr

--- vnr_report/report/report_vnr.py
+++ vnr_report/report/report_vnr.py
@@ -14,18 +14,18 @@ class ZppReportLine(models.Model):
     _description = 'This is the lines in the VNR report'
     _rec_name = 'description'
     
-    name = fields.Char('Mayor Cte',readonly=True)
+    #name = fields.Char('Mayor Cte',readonly=True)
     default_code = fields.Char('Material',readonly=True)
     description = fields.Char('Description',readonly=True)
+    product_id = fields.Many2one('product.product','Product',readonly=True)
     total_stock = fields.Float('Total Stock',readonly=True)
     total = fields.Float('Total Value',readonly=True)
     unit_price = fields.Float('Unit Value',readonly=True)
-    price_unit = fields.Float('Sale Value Unitary',readonly=True)
     sale_price = fields.Float('Sale price',readonly=True)
-    cost_by_sale = fields.Float('Cost by sale Unitary',readonly=True)
-    vnr_estimate = fields.Float('VNR estimate')
-    unit_adjustment = fields.Float('Unit Adjustment')
-    minor = fields.Float('Minor between VNR and CTO',compute="_compute_minor")
+    #cost_by_sale = fields.Float('Cost by sale Unitary',readonly=True)
+    vnr_estimate = fields.Float('VNR estimate',readonly=True)
+    unit_adjustment = fields.Float('Unit Adjustment',readonly=True)
+    #minor = fields.Float('Minor between VNR and CTO',compute="_compute_minor")
     total_adjustment = fields.Float('Total Adjustment',compute="_compute_total_adjustment")
     
     
@@ -56,11 +56,12 @@ class ZppReportLine(models.Model):
         select 
         row_number() OVER (ORDER BY pp.id) as id,
         --(
-          --  select aa.code from
-            --account_account aa letf join product_category pc 
-            --on (pc.property_account_creditor_price_difference_categ = aa.id)
-            --where pp.categ_id = pc.id
+        --    select aa.code from
+        --    account_account aa letf join product_category pc 
+        --    on (pc.property_account_creditor_price_difference_categ = aa.id)
+        --    where pp.categ_id = pc.id
         --)as name,
+        pp.id as product_id,
         pp.default_code,
         (
             select pt.name from
@@ -87,25 +88,15 @@ class ZppReportLine(models.Model):
             from product_pricelist_item ppi
             where pp.id = ppi.product_id
         )as sale_price,
-        (
-            select sum(a) 
-            --case when sum(a) <= 0 then 0
-            --else sum(a)
-            --end
-            from
-                (
-                    select (sum(ppi.cost_by_sale)/count(ppi.product_id) + (select sum(svl.value)/sum(svl.quantity) 
-                    from stock_valuation_layer svl where pp.id = svl.product_id)) as a
-                    from product_pricelist_item ppi
-                    where pp.id = ppi.product_id 
-                    
-                    UNION ALL
-                    
-                    select ((sum(ppi.cost_by_sale)/count(ppi.product_id)) * (-1)) as a
-                    from product_pricelist_item ppi
-                    where pp.id = ppi.product_id
-                ) as b
-        )as cost_by_sale,
+        --(
+        --    select (case when (sum(ppi.cost_by_sale)/count(ppi.product_id)) = 0 
+        --    then 0 
+        --    else 
+        --    ppi.cost_by_sale end) + (sum(svl.value)/sum(svl.quantity)) 
+        --    from product_pricelist_item ppi 
+        --    left join stock_valuation_layer svl on pp.id = svl.product_id 
+        --    group by ppi.product_id,ppi.cost_by_sale,svl.quantity,svl.value
+        --)as cost_by_sale,
         (
             select sum(value)
             from (
@@ -143,58 +134,63 @@ class ZppReportLine(models.Model):
         self.env.cr.execute(query)
         
     
-    def prelimit_journal(self):
+    def vnr_journal(self):
         today = datetime.now()
         date = today.strftime("%d/%m/%Y")
         account_obj = self.env['account.move']
-        prelimit_ids = self.filtered(lambda prelimit: prelimit.cost_cif != 0 and prelimit.cost_mod != 0)
+        vnr_ids = self.filtered(lambda vnr: vnr.product_id and vnr.unit_price != 0)
         _logger.error('------------------ prelimit_ids----------')
-        _logger.error(prelimit_ids)
-        for record in prelimit_ids:
-            mod = []
-            cif = []
-            if record.cost_mod and record.workcenter_id and record.production_id:
+        _logger.error(vnr_ids)
+        for record in vnr_ids:
+            _logger.error(record.product_id)
+            lines = []
+            if record.vnr_estimate < 0 and (record.product_id.categ_id.property_stock_valuation_account_id,record.product_id.categ_id.property_stock_account_output_categ_id):
                 line = {
-                        'name': record.production_id.name + ' - ' + record.workcenter_id.name+' - '+record.workcenter_id.account_analytic_real.name,
-                        'debit': record.cost_mod,
-                        'credit': 0.00,
-                        'account_id': record.workcenter_id.mod_account_id_real.id,
-                        'analytic_account_id': record.workcenter_id.account_analytic_real.id
-                        }
-                mod.append((0,0,line))
-                line = {
-                        'name': record.production_id.name + ' - ' + 'Contrapartida -' + record.workcenter_id.name+' - '+record.workcenter_id.account_analytic_real.name,
+                        'name': record.default_code,
                         'debit': 0.00,
-                        'credit': record.cost_mod,
-                        'account_id': record.workcenter_id.account_mod_id_real.id,
-                        'analytic_account_id': record.workcenter_id.account_analytic_real.id
+                        'credit': abs(record.total_adjustment),
+                        'account_id': record.product_id.categ_id.property_stock_valuation_account_id.id
+                        #'analytic_account_id': record.workcenter_id.account_analytic_real.id
                         }
-                mod.append((0,0,line))
+                lines.append((0,0,line))
+                line = {
+                        'name': record.default_code,
+                        'debit': abs(record.total_adjustment),
+                        'credit': 0.00,
+                        'account_id': record.product_id.categ_id.property_stock_account_output_categ_id.id
+                        #'analytic_account_id': record.workcenter_id.account_analytic_real.id
+                        }
+                lines.append((0,0,line))
+            elif record.vnr_estimate >= 0 and (record.product_id.categ_id.property_stock_valuation_account_id,record.product_id.categ_id.property_stock_account_output_categ_id):
+                line = {
+                        'name': record.default_code,
+                        'debit': abs(record.total_adjustment),
+                        'credit': 0.00,
+                        'account_id': record.product_id.categ_id.property_stock_valuation_account_id.id
+                        #'analytic_account_id': record.workcenter_id.account_analytic_real.id
+                        }
+                lines.append((0,0,line))
+                line = {
+                        'name': record.default_code,
+                        'debit': 0.00,
+                        'credit': abs(record.total_adjustment),
+                        'account_id': record.product_id.categ_id.property_stock_account_output_categ_id.id
+                        #'analytic_account_id': record.workcenter_id.account_analytic_real.id
+                        }
+                lines.append((0,0,line))
             else:
-                raise ValidationError(('Costos MOD debe ser diferente de Cero'))
-            if record.cost_cif and record.workcenter_id and record.production_id:
-            acc_move_ids = []
-            if mod:
-                move = {
-                        'journal_id': record.production_id.product_id.categ_id.property_stock_journal.id,
-                        'line_ids': mod,
-                        'date': fields.Date.today(),
-                        'ref': record.production_id.name + ' - MOD'+' - '+record.workcenter_id.account_analytic_real.name,
-                        'type': 'entry',
-                        'prelimit_id': record.id
-                        }
-                account_move = account_obj.sudo().create(move)
-                acc_move_ids.append(account_move.id)
-            if cif:
-                move = {
-                        'journal_id': record.production_id.product_id.categ_id.property_stock_journal.id,
-                        'line_ids': cif,
-                        'date': fields.Date.today(),
-                        'ref': record.production_id.name + ' - CIF'+' - '+record.workcenter_id.account_analytic_real.name,
-                        'type': 'entry',
-                        'prelimit_id': record.id
-                        }
-                account_move = account_obj.sudo().create(move)
-                acc_move_ids.append(account_move.id)
+                raise ValidationError(('La cuenta no esta definida en la categoria del producto'))
+            if record.product_id:
+                acc_move_ids = []
+                if lines:
+                    move = {
+                            'journal_id': record.product_id.categ_id.property_stock_journal.id,
+                            'line_ids': lines,
+                            'date': fields.Date.today(),
+                            'ref': record.product_id.name+'-'+record.default_code,
+                            'type': 'entry',
+                            }
+                    account_move = account_obj.sudo().create(move)
+                    acc_move_ids.append(account_move.id)
         res = {'type': 'ir.actions.client','tag': 'display_notification','params': {'title': _('Warning!'),'message': 'Asientos creados con exito','sticky': False, }}
         return res
